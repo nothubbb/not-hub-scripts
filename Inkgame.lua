96c5b74571525b42b9d05c1ccd614fd9f58f639f
Not hub <fluxoxsensi@gmail.com>
2025-11-25 05:28:40 +0530

    Update Inkgame.lua

--- Inkgame.lua
+++ Inkgame.lua
@@ -136,7 +136,7 @@ StatusLabel.TextSize = 11
 StatusLabel.Parent = MainFrame
 
 -- Configuration
-local DISCORD_INVITE = "https://discord.gg/sVftr5rs"
+local DISCORD_INVITE = "https://discord.gg/gBxyGjqE"
 
 -- Dragging functionality
 local UIS = game:GetService("UserInputService")
